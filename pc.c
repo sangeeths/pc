2513b966ab903404c304f564b5ce1c2ee7a68af5
Sangeeth Saravanaraj <sangeeth.saravanaraj@gmail.com>
2011-12-17 22:04:05 +0530

    basic working version

--- pc.c
+++ pc.c
@@ -3,73 +3,164 @@
 #include <string.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
+#include <pthread.h>
 
 #include "pc-cmd.h"
 #include "q.h"
 
+queue *q = NULL;
+pthread_mutex_t q_mutex;
+
+void * consume(void *id)
+{
+    int *tid = (int *) id;
+    int q_data;
+    int deq_ret_val;
+
+    while (1) {
+        pthread_mutex_lock(&q_mutex);
+
+        deq_ret_val = dequeue(q, &q_data);
+
+        if (deq_ret_val == QEMPTY)
+            printf("%3d : queue empty  :  ", *tid);
+
+        if (deq_ret_val == Q_OP_SUCCESS)
+            printf("%3d : consumed %4d :  ", *tid, q_data);
+
+        display_q(q);
+
+        pthread_mutex_unlock(&q_mutex);
+        sleep(2);
+    }
+
+    return;
+}
+
+
+//void produce(void *id, queue *q, pthread_mutex_t *q_mutex)
+void * produce(void *id)
+{
+    int *tid = (int *) id;
+    int enq_ret_val;
+    int item = rand() % 99 + 1;
+
+    while (1) {
+        pthread_mutex_lock(&q_mutex);
+
+        enq_ret_val = enqueue(q, item);
+
+        if (enq_ret_val == QFULL)
+            printf("%3d : queue full   :  ", *tid);
+
+        if (enq_ret_val == Q_OP_SUCCESS)
+            printf("%3d : produced %4d :  ", *tid, item);
+
+        display_q(q);
+
+        pthread_mutex_unlock(&q_mutex);
+        sleep(2);
+    }
+
+    return;
+}
 
 int main(int argc, char *argv[])
 {
+    int t_id_i = 0;
+    int i;
     /* command line arguments - parsed */
     pc_cmd *pc = (pc_cmd *) malloc(sizeof(pc_cmd));
 
     /* global queue */
-    queue *q = NULL;
-    queue *shared_q = NULL;
     int qreturn = 0;
     int dq_data; 
+    
+    pthread_mutex_init(&q_mutex, NULL);
 
     if (parse_cmdline_args(&pc, argc, argv) == PARSE_FAILURE) {
         usage();
-        goto DEALLOCATE_AND_EXIT;
+        if (pc) free(pc);
+        return 0;
     }
 
     /* set the debug mode to the global debug flag */
     g_debug = pc->dbg;
-    PRINT_INFO("cmdline args parsed successfully");
-    print_cmdline_args(pc); 
-
 
     /* create queue */
     if (create_q(&q, pc->qs) != Q_OP_SUCCESS) {
         printf("ERROR: unable to create queue \n");
-        goto DEALLOCATE_AND_EXIT;
+        if (pc) free(pc);
+        return 0;
     }
 
-    PRINT_INFO("queue d created successfully");
+    /* initialize mutex */
+	pthread_mutex_init(&q_mutex, NULL);	
+
+    if ((t_id = (int *) malloc (sizeof(int) * 
+                (pc->nc + pc->np))) == NULL) {
+        printf("ERROR: unable to create ids \n");
+        if (pc) free(pc);
+        delete_q(&q);
+        return 0;
+    }
+
+	for (i=0 ; i < (pc->np + pc->nc) ; i++) 
+        t_id[i] = i;
+
+    /* create producer thread ids */
+    if ((p_id = (pthread_t *) malloc(sizeof(pthread_t) * 
+                 pc->np)) == NULL) {
+        printf("ERROR: unable to create producer id\n");
+        if (pc) free(pc);
+        delete_q(&q);
+        return 0;
+    }
 
 
-    /* Allocate a shared memory segment.  */ 
-    shm_id = shmget (IPC_PRIVATE, sizeof(*q),
-                     IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR); 
+    /* create consumer thread ids */
+    if ((c_id = (pthread_t *) malloc(sizeof(pthread_t) * 
+                 pc->nc)) == NULL) {
+        printf("ERROR: unable to create consumer id\n");
+        if (pc) free(pc);
+        delete_q(&q);
+        return 0;
+    }
 
-    shared_q = (queue *) shmat (shm_id, 0, 0); 
-     
-    shared_q->front = shared_q->rear = QNULL;
-    shared_q->size = QUEUE_SIZE;  
+    /* create producer threads */
+	for (i=0 ; i<pc->np ; i++) {
+        printf("Creating producer thread #%d \n", t_id[t_id_i]);
+        if (pthread_create(&p_id[i], NULL, produce, &t_id[t_id_i])) {
+			printf("unable to create producer thread! \n");
+			exit(-1);
+		} 
+        t_id_i++; 
+	}
 
-    enqueue(shared_q, 10);
-    enqueue(shared_q, 20);
-    enqueue(shared_q, 30);
-    enqueue(shared_q, 40);
-    dequeue(shared_q, &dq_data);
-    enqueue(shared_q, 50);
-    enqueue(shared_q, 60);
-    enqueue(shared_q, 70);
-    dequeue(shared_q, &dq_data);
-    display_q(shared_q);
-    //memcpy(shared_q, q, sizeof(*q));
-    /*
-    printf("shared_q->front     :   %d \n", shared_q->front);
-    printf("shared_q->rear      :   %d \n", shared_q->rear);
-    printf("shared_q->size      :   %d \n", shared_q->size);
-    printf("shared_q->data      :   %p \n", shared_q->data);
-    */
+    /* create consumer threads */
+	for (i=0 ; i < pc->nc ; i++) {
+        printf("Creating consumer thread #%d \n", t_id[t_id_i]);
+        if (pthread_create(&c_id[i], NULL, consume, &t_id[t_id_i])) {
+			printf("unable to create consumer thread! \n");
+			exit(-1);
+		} 
+        t_id_i++;
+	}
 
-DEALLOCATE_AND_EXIT:    
-    /* deallocate command line args */
-    if (q)  free(q);
-    if (pc) free(pc);
+	for (i=0 ; i < pc->np ; i++) 
+		pthread_join(p_id[i], NULL);
+	for (i=0 ; i < pc->nc ; i++) 
+		pthread_join(c_id[i], NULL);
+	
+
+    /* delete queue */
+    delete_q(&q);
+
+    /* destroy mutex */
+	pthread_mutex_destroy(&q_mutex);
+
+    /* exit all threads */
+	pthread_exit(NULL);	
 
     return 0;
 }
